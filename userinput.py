0b176113995bf33894bcb4735cf63ac0ae14f9fa
Saljesh <saljesh2020@gmail.com>
2022-03-22 21:18:23 +0545

    calculator

---
+++ userinput.py
@@ -0,0 +1,17 @@
+print("Enter two number\"s\n")
+a = int(input("Enter First Number\n"))
+b = int(input("Enter Second Number\n"))
+
+def add():
+    return a + b
+print('Sum is ' , add())
+
+def sub():
+    return a - b
+print('Difference is ', sub())
+
+def mul():
+    return a * b
+print('Multiplication is ', mul())    
+
+
